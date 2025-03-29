68bfcfe405836e0a5f355a97f591e23f90d4931b
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-03-29 15:40:06 -0300

    Update ConversorTemperatura.java

--- ConversorTemperatura.java
+++ ConversorTemperatura.java
@@ -1,23 +1,37 @@
 public class ConversorTemperatura {
-     // características da classe
-     double celsius;
-     double fahrenheit;
     
-    // funções construtoras
-    //public ConversorTemperatura() {}; // construtor padrão
+   public ConversorTemperatura() {}; // construtor padrão
 
+   // métodos/funções
 
-    // métodos/funções
-    public double conversor (celsius) {   // Celsius para Fahrenheit
-        double resultado = celsius * 1.8 + 32.0;
-            return resultado;
+   public double conversor (int celsius) {   // Celsius para Fahrenheit
+       double resultado = (celsius * 9.0/5.0) + 32.0;
+           return resultado;
+   }
+
+   public double conversor (double fahrenheit) {   // Fahrenheit para Celsius
+       double resultado = (fahrenheit - 32.0)/1.8;
+           return resultado;
+   }
+  
+   public double conversor (int celsius, double ajuste, boolean arredondamento) {   // Celsius para Fahrenheit + ajuste +arredondamento
+        double resultado = (celsius * 9.0/5.0) + 32.0 + ajuste;
+     // Verifica se o valor deve ser arredondado
+     if (arredondamento = true) {
+        resultado = Math.round(resultado); // Arredonda para o inteiro mais próximo
     }
+        return resultado;
+  }
 
-    public double conversor (fahrenheit) {   // Fahrenheit para Celsius
-        double resultado = (fahrenheit-32.0)/1.8;
-            return resultado;
+  public double conversor (double fahrenheit, double ajuste, boolean arredondamento) {   // Fahrenheit para Celsius+ ajuste +arredondamento
+        double resultado = (fahrenheit - 32.0)/1.8 + ajuste;
+     // Verifica se o valor deve ser arredondado
+     if (arredondamento = true) {
+        resultado = Math.round(resultado); // Arredonda para o inteiro mais próximo
     }
+        return resultado;
+  }
    
-    
 }
-
+// obs: o valor em graus Celsius também poderia ser um double, mas daí não daria certo para fazer 
+// a sobrecarga pois as assinaturas ficariam iguais, então eu deixei como int
