794480ba4066a5a10548f86fa6cc671899db121f
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-03-29 15:40:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,9 @@ public class Main{
         Scanner lerOpcao = new Scanner(System.in);
         ConversorTemperatura converter = new ConversorTemperatura();
         int opcao;
+        boolean arredondar;
+        String resposta;
+        double ajuste;
     
         do { 
         
@@ -13,34 +16,71 @@ public class Main{
 
         System.out.println("Digite o número da opção desejada:");
         opcao = lerOpcao.nextInt();
-        //lerOpcao.nextLine(); // coloquei e deu certo para ler e apresentar o nome
+        lerOpcao.nextLine();
 
             switch(opcao){
                 case 1:
                     System.out.println("Digite o valor em graus Celsius:"); // entrada de dados
-                    converter.celsius(lerOpcao.nextDouble());
+                        int valor = (lerOpcao.nextInt());
                     lerOpcao.nextLine(); // ler o enter do teclado     
                 
-                    System.out.println("Temperatura em Fahrenheit: " + converter.conversor() );
+                    System.out.println("Temperatura em Fahrenheit: " + converter.conversor(valor) );
                 break; 
 
-                // case 2:
-                //     System.out.println("Digite o novo nome:"); // entrada de novos dados
-                //     pessoa1.setNome(lerOpcao.nextLine());
-                //     lerOpcao.nextLine();
+                case 2:
+                    System.out.println("Digite o valor em graus Fahrenheit:"); // entrada de dados
+                        double valor1 = (lerOpcao.nextDouble());
+                    lerOpcao.nextLine(); // ler o enter do teclado     
                 
-                //     System.out.println("Digite a nova idade:");
-                //     pessoa1.setIdade(lerOpcao.nextInt());
-                //     lerOpcao.nextLine();
-
-                //     System.out.println("Digite o novo e-mail:");
-                //     pessoa1.setEmail(lerOpcao.nextLine());
-                //     lerOpcao.nextLine();
-
-                //     System.out.println("Dados atualizados com sucesso!:) ");
-                // break; 
+                    System.out.println("Temperatura em Celsius: " + converter.conversor(valor1) );
+                break; 
 
                 case 3:
+                    System.out.println("Digite o valor em graus Celsius:"); // entrada de dados
+                        int valor2 = (lerOpcao.nextInt());
+                    lerOpcao.nextLine(); // ler o enter do teclado     
+
+                    System.out.println("Digite o valor do ajuste:"); 
+                        ajuste = (lerOpcao.nextDouble());
+                    lerOpcao.nextLine(); // ler o enter do teclado     
+
+                    System.out.println("Deseja arredondar? (digite sim ou não):"); 
+                        resposta = (lerOpcao.nextLine());
+                    lerOpcao.nextLine(); // ler o enter do teclado   
+                    
+                    if(resposta == "sim"){
+                        arredondar = true;
+                        System.out.println("Temperatura em Fahrenheit, com ajuste de " + ajuste+ " e arredondamento :"+ converter.conversor(valor2,ajuste,arredondar) );
+                    } else {
+                        arredondar = false;
+                        System.out.println("Temperatura em Fahrenheit, com ajuste de " + ajuste+ " :"+ converter.conversor(valor2,ajuste,arredondar) );
+                    }
+                break;
+            
+                case 4:
+                    System.out.println("Digite o valor em graus Fahrenheit:"); // entrada de dados
+                        double valor3 = (lerOpcao.nextDouble());
+                    lerOpcao.nextLine(); // ler o enter do teclado     
+
+                    System.out.println("Digite o valor do ajuste:");
+                        ajuste = (lerOpcao.nextDouble());
+                    lerOpcao.nextLine(); // ler o enter do teclado     
+
+                    System.out.println("Deseja arredondar? (digite sim ou não):"); 
+                        resposta = (lerOpcao.nextLine());
+                    lerOpcao.nextLine(); // ler o enter do teclado   
+                    
+                    if(resposta == "sim"){
+                        arredondar = true;
+                        System.out.println("Temperatura em Celsius, com ajuste de " + ajuste+ " e arredondamento :"+ converter.conversor(valor3,ajuste,arredondar) );
+                    } else {
+                        arredondar = false;
+                        System.out.println("Temperatura em Celsius, com ajuste de " + ajuste+ " :"+ converter.conversor(valor3,ajuste,arredondar) );
+                    }
+                    
+                break; 
+
+                case 5:
                     System.out.println("Você saiuu...:");     
                 break; 
 
@@ -49,14 +89,17 @@ public class Main{
                 break;
             }
             
-        } while(opcao!= 3); // repetir enquanto não digita a opção 3
-    }
-
+        } while(opcao!= 5); // repetir enquanto não digita a opção 5
+    } 
         private static void listaDeOpcoes(){
         System.out.println("---------------------------------------------------------------------------------");
         System.out.println("Oie, seja bem vind@ ao sistema de Conversão de Temperatura! Escolha uma operação:");
         System.out.println("1 - Converter temperatura em Celsius para Fahrenheit:");
         System.out.println("2 - Converter temperatura em Fahrenheit para Celsius:");
-        System.out.println("3 - Sair do sistema!");
-        }
+        System.out.println("3 - Converter temperatura em Celsius para Fahrenheit, com ajuste extra e arredondamento");
+        System.out.println("(ex: 30°C em F° = 86.0, com ajuste de 2 e arredondamento = 88.0) :");
+        System.out.println("4 - Converter temperatura em Fahrenheit para Celsius, com ajuste extra e arredondamento");
+        System.out.println("(ex: 53°F em C° = 11.6667, com ajuste de 2 e arredondamento = 14.0) :");
+        System.out.println("5 - Sair do sistema!");
+        }     
 }
